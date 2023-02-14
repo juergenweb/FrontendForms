3f6870713c9c421c1a37b1856a02aa6f6a0160be
juergenweb <juergenweb@users.noreply.github.com>
2023-02-14 16:58:21 +0100

    Update fileuploadtopage.php

--- Examples/fileuploadtopage.php
+++ Examples/fileuploadtopage.php
@@ -18,11 +18,35 @@ namespace ProcessWire;
  * Fe you have to use $form = new \FrontendForms\Form('myForm'); or $form = new FrontendForms\Form('myForm'); and not only $form = new Form('myForm');
  */
 
+<?php
+declare(strict_types=1);
+
+namespace ProcessWire;
+/*
+ * File description
+ *
+ * Created by Jürgen K.
+ * https://github.com/juergenweb 
+ * File name: fileuploadtopage.php
+ * Created: 13.02.2023 
+ */
+
+/*
+ * Demonstration of a simple file upload form
+ * You can copy this code to a template to show the form
+ * If you use these classes inside another namespace (fe ProcessWire) like in this case, you have to take care about the namespace in front of the class instances.
+ * Fe you have to use $form = new \FrontendForms\Form('myForm'); or $form = new FrontendForms\Form('myForm'); and not only $form = new Form('myForm');
+ */
+
 echo  '<h2>A simple working file upload form for uploading files to a page</h2>';
+
+$folder = $page->id; // set page id as the folder name
+
+echo '<p>Files will be uploaded to site/assets/files/'.$folder.'</p>';
 
 $form = new \FrontendForms\Form('upload');
 $form->setAttribute('enctype', 'multipart/form-data');
-$form->setUploadPath($page->id, true);
+$form->setUploadPath($folder, true);
 
 $file1 = new \FrontendForms\InputFile('fileupload1');
 $file1->setLabel('Multiple files upload');
@@ -45,7 +69,7 @@ echo $form->render();
 
 $page_files = $files->find($config->paths->assets.'files/'.$page->id);
 if($page_files){
-    echo '<p>List of page files:</p>';
+    echo '<p>List of page files inside the folder '.$folder.':</p>';
     echo '<ul>';
     foreach($page_files as $value){
         echo '<li>'.$value.'</li>';
