7a1a1a254128b57c09186efb5e811924166ce85f
juergenweb <juergenweb@users.noreply.github.com>
2023-02-22 16:34:48 +0100

    Adding HTML5 attributes depending on validation rules

--- Formelements/Inputelements/Inputs/InputText.php
+++ Formelements/Inputelements/Inputs/InputText.php
@@ -31,6 +31,41 @@ class InputText extends Input
      */
     public function ___renderInputText(): string
     {
+        // create HTML5 minlength attribute depending on validator settings
+        if(array_key_exists('lengthMin',$this->notes_array)){
+            $this->setAttribute('minlength', (string)$this->notes_array['lengthMin']);
+        }
+
+        // create HTML5 max attribute depending on validator settings
+        if(array_key_exists('lengthMax',$this->notes_array)){
+            $this->setAttribute('maxlength', (string)$this->notes_array['lengthMax']);
+        }
+
+        // do not add these patterns on input tel
+        if(get_class($this) != 'FrontendForms\InputTel') {
+
+            // create HTML5 pattern attribute for alphabetical characters only depending on validator settings
+            if(array_key_exists('alpha',$this->notes_array)){
+                $this->setAttribute('pattern', '[a-zA-Z]+');
+            }
+
+            // create HTML5 pattern attribute for alphanumeric characters only depending on validator settings
+            if(array_key_exists('alphaNum',$this->notes_array)){
+                $this->setAttribute('pattern', '[A-Za-z0-9]+');
+            }
+
+            // create HTML5 pattern attribute for ascii characters only depending on validator settings
+            if(array_key_exists('ascii',$this->notes_array)){
+                $this->setAttribute('pattern', '[\x00-\x7F]+');
+            }
+
+            // create HTML5 pattern attribute for slug characters only depending on validator settings
+            if(array_key_exists('slug',$this->notes_array)){
+                $this->setAttribute('pattern', '[A-Za-z0-9-_]+');
+            }
+
+        }
+
         return $this->renderInput();
     }
 
