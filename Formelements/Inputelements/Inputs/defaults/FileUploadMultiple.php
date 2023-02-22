ed1713b320f42055e7acfffd52e1774b9613fab6
juergenweb <juergenweb@users.noreply.github.com>
2023-02-22 16:46:54 +0100

    Add files via upload

---
+++ Formelements/Inputelements/Inputs/defaults/FileUploadMultiple.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+
+namespace FrontendForms;
+
+/*
+ * Pre-defined file upload field for multiple files
+ *
+ * Created by Jürgen K.
+ * https://github.com/juergenweb 
+ * File name: MultipleFileUpload.php
+ * Created: 19.02.2023 
+ */
+
+
+use Exception;
+
+class FileUploadMultiple extends InputFile
+{
+
+    /**
+     * @param string $id
+     * @throws Exception
+     */
+    public function __construct(string $id = 'multi-upload')
+    {
+        parent::__construct($id);
+        $this->setLabel($this->_('Upload multiple files'));
+    }
+
+    /**
+     * Render the file upload input field
+     * @return string
+     */
+    public function ___renderFileUploadMultiple(): string
+    {
+        return parent::___renderInputFile();
+    }
+
+}
---
+++ Formelements/Inputelements/Inputs/defaults/FileUploadSingle.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+
+namespace FrontendForms;
+
+/*
+ * Pre-defined file upload field for a single file
+ *
+ * Created by Jürgen K.
+ * https://github.com/juergenweb 
+ * File name: FileUpload.php
+ * Created: 15.02.2023 
+ */
+
+use Exception;
+
+class FileUploadSingle extends InputFile
+{
+
+    /**
+     * @param string $id
+     * @throws Exception
+     */
+    public function __construct(string $id = 'single-upload')
+    {
+        parent::__construct($id);
+        $this->setMultiple(false);
+        $this->setLabel($this->_('Upload single file'));
+    }
+
+    /**
+     * Render the file upload input field
+     * @return string
+     */
+    public function ___renderFileUploadSingle(): string
+    {
+        return parent::___renderInputFile();
+    }
+
+}
