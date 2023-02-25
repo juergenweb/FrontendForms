63404b9a0e8db1ce098f5a4e0e899811c321579e
juergenweb <juergenweb@users.noreply.github.com>
2023-02-25 18:55:58 +0100

    Correct problems by validating files

--- valitron/customValidators.php
+++ valitron/customValidators.php
@@ -14,26 +14,34 @@ namespace FrontendForms;
 
 use ProcessWire\User as User;
 use function ProcessWire\wire;
+use Valitron\Validator as V;
+
+
+// set path to the language folder
+V::langDir($this->wire('config')->paths->siteModules . 'FrontendForms/valitron/');
+// set language for the error messages to use the errormessage.php inside valitron folder
+V::lang('errormessages');
 
 /**
  * Get the id of the form where the field is included
  * @return string|bool
  */
-function getFormId(): string|bool
+function getFormId():string|bool
 {
+
     $request_method = strtolower(wire('input')->requestMethod());
     $values = wire('input')->$request_method();
     // grab the form key containing the form id
     $id = '';
-    foreach($values as $key => $fieldvalue){
+    foreach ($values as $key => $fieldvalue) {
         $needle = substr($key, -8);
-        if($needle == '-form_id'){
+        if ($needle == '-form_id') {
             $id = $fieldvalue;
             break;
         }
     }
     // if id is not present, what should not be, set it to false in any case
-    if($id == ''){
+    if ($id == '') {
         return false;
     }
     return $id;
@@ -44,19 +52,20 @@ function getFormId(): string|bool
  * @param string $field_name
  * @return string
  */
-function getFieldName(string $field_name): string
+function getFieldName(string $field_name):string
 {
-   $form_id = getFormId();
+    $form_id = getFormId();
     // check if form id is used as the pre-fix of the field name
-    if(!substr($field_name, strlen($form_id))){
+    if (!substr($field_name, strlen($form_id))) {
         // prefix is not added, so add it to the field name
-        $field_name = $form_id.'-'.$field_name;
+        $field_name = $form_id . '-' . $field_name;
     }
     return $field_name;
 }
 
+
 // 1) Check if username is unique
-$this->Validator::addRule('uniqueUsername', function ($value) {
+V::addRule('uniqueUsername', function ($field, $value) {
     // check if user is logged in
     if ($this->wire('user')->isLoggedin()) {
         if ($this->wire('user')->name === $value) {
@@ -64,6 +73,7 @@ $this->Validator::addRule('uniqueUsername', function ($value) {
         }
     }
     $value = $this->wire('sanitizer')->pageName($value);
+    bd($value);
     if ($this->wire('users')->get($value)->id !== 0) {
         return false;
     }
@@ -72,12 +82,13 @@ $this->Validator::addRule('uniqueUsername', function ($value) {
 
 
 // 2) Check if username and password match
-$this->Validator::addRule('matchUsername', function ($field, $value, array $params) {
-
+V::addRule('matchUsername', function ($field, $value, array $params) {
     $fieldName = getFieldName($params[0]); // get field name including form id prefix
+
     $username = $this->wire('input')->$fieldName;
     $username = $this->wire('sanitizer')->pageName($username); // important
     $u = $this->wire('users')->get($username);
+    bd($value);
     if ($u->id && $u->pass->matches($value)) {
         return true;
     }
@@ -90,7 +101,7 @@ $this->Validator::addRule('matchUsername', function ($field, $value, array $para
 * fe. 8 characters long, must contain number, must contain upper and lowercase...
 * Add field name as first param if field name is not pass
 */
-$this->Validator::addRule('meetsPasswordConditions', function ($field, $value, array $params) {
+V::addRule('meetsPasswordConditions', function ($field, $value, array $params) {
 // grab the InputfieldPassword object
     $passwordModule = $this->wire('modules')->get('InputfieldPassword');
 // get the field object
@@ -113,7 +124,7 @@ $this->Validator::addRule('meetsPasswordConditions', function ($field, $value, a
 /*
 * 4) Check if username contains only the following characters: a-z0-9-_
 */
-$this->Validator::addRule('usernameSyntax', function ($field, $value) {
+V::addRule('usernameSyntax', function ($field, $value) {
     $regex = '/[a-z\d\-_.]$/';
     if (preg_match($regex, $value)) {
         return true;
@@ -124,7 +135,7 @@ $this->Validator::addRule('usernameSyntax', function ($field, $value) {
 
 
 // 5) Check if email is unique
-$this->Validator::addRule('uniqueEmail', function ($value) {
+V::addRule('uniqueEmail', function ($field, $value) {
     // check if user is logged in
     if ($this->wire('user')->isLoggedin()) {
         // user is logged in
@@ -147,7 +158,7 @@ $this->Validator::addRule('uniqueEmail', function ($value) {
  * As param enter the user object
  * $field->setRule('checkPasswordOfUser', $user);
  */
-$this->Validator::addRule('checkPasswordOfUser', function ($field, $value, array $params) {
+V::addRule('checkPasswordOfUser', function ($field, $value, array $params) {
     $user = $params[0];
     if ($user instanceof User) {
         return $user->pass->matches($value);
@@ -159,7 +170,7 @@ $this->Validator::addRule('checkPasswordOfUser', function ($field, $value, array
 /*
 * 7) Check if email and password match
 */
-$this->Validator::addRule('matchEmail', function ($field, $value, array $params) {
+V::addRule('matchEmail', function ($field, $value, array $params) {
     $fieldName = getFieldName($params[0]); // get field name including form id prefix
     $email = $this->wire('input')->$fieldName;
     $u = $this->wire('users')->get('email=' . $email);
@@ -173,7 +184,7 @@ $this->Validator::addRule('matchEmail', function ($field, $value, array $params)
 /*
 * 8) Check if value is boolean true
 */
-$this->Validator::addRule('isBooleanAndTrue', function ($field, $value) {
+V::addRule('isBooleanAndTrue', function ($field, $value) {
     if (is_bool($value)) {
         if ($value) {
             return true;
@@ -187,7 +198,7 @@ $this->Validator::addRule('isBooleanAndTrue', function ($field, $value) {
 /*
 * 9) Check if value is boolean false
 */
-$this->Validator::addRule('isBooleanAndFalse', function ($field, $value) {
+V::addRule('isBooleanAndFalse', function ($field, $value) {
     return !(is_bool($value) && $value);
 }, $this->_('is not Boolean false'));
 
@@ -195,7 +206,7 @@ $this->Validator::addRule('isBooleanAndFalse', function ($field, $value) {
 /*
 * 10) Check if the value is exact the same value as entered as param
 */
-$this->Validator::addRule('exactValue', function ($field, $value, array $params) {
+V::addRule('exactValue', function ($field, $value, array $params) {
     return ($value === $params[0]);
 }, $this->_('does not have the expected value'));
 
@@ -210,7 +221,7 @@ $this->Validator::addRule('exactValue', function ($field, $value, array $params)
  * [1]: Module: The Tfa module object which is used by the user
  * @return boolean
  */
-$this->Validator::addRule('checkTfaCode', function ($field, $value, array $params) {
+V::addRule('checkTfaCode', function ($field, $value, array $params) {
     $user = $params[0]; // user object
     $module = $params[1]; // module object
     $className = $module->className(true);
@@ -224,7 +235,7 @@ $this->Validator::addRule('checkTfaCode', function ($field, $value, array $param
  * 12) Check if the value entered is different from another value entered as param
  * $field->setRule('differentValue', 'test'); // entered value must be different from "test" to return true
  */
-$this->Validator::addRule('differentValue', function ($field, $value, array $params) {
+V::addRule('differentValue', function ($field, $value, array $params) {
     return ($value !== $params[0]);
 }, $this->_('entered must be different than the value specified.'));
 
@@ -233,7 +244,7 @@ $this->Validator::addRule('differentValue', function ($field, $value, array $par
  * 13) Check if the new password entered is different the old one
  * $field->setRule('differentPassword', $user);
  */
-$this->Validator::addRule('differentPassword', function ($field, $value, array $params) {
+V::addRule('differentPassword', function ($field, $value, array $params) {
     if ($params[0]->pass->matches($value)) {
         return false;
     }
@@ -245,14 +256,14 @@ $this->Validator::addRule('differentPassword', function ($field, $value, array $
  * 14) Check if captcha value is correct
  * This is a special validator for the integrated captcha field
  */
-$this->Validator::addRule('checkCaptcha', function ($field, $value, array $params) {
+V::addRule('checkCaptcha', function ($field, $value, array $params) {
     return ($value === $params[0]);
 }, $this->_('value entered is not correct.'));
 
 /**
  * 15) Check if password is safe - in other words: is not in the list of the most popular passwords
  */
-$this->Validator::addRule('safePassword', function ($field, $value) {
+V::addRule('safePassword', function ($field, $value) {
     $passwords = $this->wire('modules')->getConfig('FrontendForms')['input_blacklist'];
     $passwords = explode("\n", $passwords);
     if (!$passwords) {
@@ -264,57 +275,61 @@ $this->Validator::addRule('safePassword', function ($field, $value) {
 /**
  * 16) Check if the uploaded file is not larger than the allowed filesize
  */
-$this->Validator::addRule('allowedFileSize', function ($field, $value, array $params) {
-    if (count($value) == count($value, COUNT_RECURSIVE)) {
-        // one dimensional array = single upload
-        $value = [$value];
-    }
-    foreach ($value as $file) {
-        if (($file['error'] == '4') || ($file['error'] == '0')) {
+V::addRule('allowedFileSize', function ($field, $value, array $params) {
+    if (count($value) == 1) {
+        if($value[0]['error'] == '0') {
+            $size = $value[0]['size'];
+            return ($size <= $params[0]);
+        }
+    } else {
+        foreach ($value as $file) {
+            if($file['error'] == '0') {
+                $size = $file['size'];
+                return ($size <= $params[0]);
+            }
             return true;
         }
-        return ($file['size'] <= $params[0]);
     }
+    return true;
 }, $this->_('is larger than the allowed filesize of %s.'));
 
 
 /**
  * 17) Check if an error occur during the upload of the file
  */
-$this->Validator::addRule('noErrorOnUpload', function ($field, $value) {
-
-    if (count($value) == count($value, COUNT_RECURSIVE)) {
-        // one dimensional array = single upload
-
-        $value = array_filter([$value]);
-    }
-
-    foreach ($value as $file) {
-        if (($file['error'] == '4') || ($file['error'] == '0')) {
+V::addRule('noErrorOnUpload', function ($field, $value) {
+    if (count($value) == 1) {
+        $error = $value[0]['error'];
+        if (($error == '4') || ($error == '0')) {
             return true;
         }
-        return $file['error'];
+    } else {
+        foreach ($value as $file) {
+            if (($file['error'] == '4') || ($file['error'] == '0')) {
+                return true;
+            }
+            return $file['error'];
+        }
     }
-    return true;
+    return false;
 }, $this->_('caused an error during the upload.'));
 
 /**
  * 18) Check if uploaded files are of the allowed extensions
  */
-$this->Validator::addRule('allowedFileExt', function ($field, $value, array $params) {
-    // check if single or multiple file upload
-    if ($value) {
-        if (count($value) == count($value, COUNT_RECURSIVE)) {
-            // one dimensional array = single upload
-            $value = [$value];
+V::addRule('allowedFileExt', function ($field, $value, array $params) {
+    if (count($value) == 1) {
+        if($value[0]['error'] == '0'){
+            $type = strtolower(pathinfo($value[0]['name'], PATHINFO_EXTENSION));
+            return (in_array($type, $params[0]));
         }
-        $allowedExt = array_map('strtolower', $params[0]); // convert all values to lowercase
+    } else {
         foreach ($value as $file) {
-            if (($file['error'] == '4') || ($file['error'] == '0')) {
-                return true;
+            if($file['error'] == '0') {
+                $type = strtolower(pathinfo($file['name'], PATHINFO_EXTENSION));
+                return (in_array($type, $params[0]));
             }
-            $ext = strtolower(pathinfo($file['name'], PATHINFO_EXTENSION)); // convert value to lowercase
-            return (in_array($ext, $allowedExt));
+            return true;
         }
     }
     return true;
@@ -324,17 +339,19 @@ $this->Validator::addRule('allowedFileExt', function ($field, $value, array $par
 /**
  * 20) Check if the uploaded file is not larger than the allowed filesize as declared inside the php.ini
  */
-$this->Validator::addRule('phpIniFilesize', function ($field, $value) {
-    if ($value) {
-        if (count($value) == count($value, COUNT_RECURSIVE)) {
-            // one dimensional array = single upload
-            $value = [$value];
+V::addRule('phpIniFilesize', function ($field, $value) {
+    if (count($value) == 1) {
+        if($value[0]['error'] == '0') {
+            $size = $value[0]['size'];
+            return ($size <= ((int)ini_get("upload_max_filesize") * 1024));
         }
+    } else {
         foreach ($value as $file) {
-            if (($file['error'] == '4') || ($file['error'] == '0')) {
-                return true;
+            if($file['error'] == '0') {
+                $size = $file['size'];
+                return ($size <= ((int)ini_get("upload_max_filesize") * 1024));
             }
-            return ($file['size'] <= ((int)ini_get("upload_max_filesize") * 1024));
+            return true;
         }
     }
     return true;
@@ -344,27 +361,28 @@ $this->Validator::addRule('phpIniFilesize', function ($field, $value) {
  * 21) Check if uploaded files are not of the forbidden extensions
  * This is the opposition of the allowedFileExt
  */
-$this->Validator::addRule('forbiddenFileExt', function ($field, $value, array $params) {
-    // check if single or multiple file upload
-    if (count($value) == count($value, COUNT_RECURSIVE)) {
-        // one dimensional array = single upload
-        $value = [$value];
-    }
-    $forbiddenExt = array_map('strtolower', $params[0]); // convert all values to lowercase
-    foreach ($value as $file) {
-        if ($file['name']) {
-            $ext = strtolower(pathinfo($file['name'], PATHINFO_EXTENSION)); // convert value to lowercase
-            return (!in_array($ext, $forbiddenExt));
-        } else {
+V::addRule('forbiddenFileExt', function ($field, $value, array $params) {
+    if (count($value) == 1) {
+        if($value[0]['error'] == '0') {
+            $type = strtolower(pathinfo($value[0]['name'], PATHINFO_EXTENSION));
+            return (!in_array($type, $params[0]));
+        }
+    } else {
+        foreach ($value as $file) {
+            if($file['error'] == '0') {
+                $type = strtolower(pathinfo($file['name'], PATHINFO_EXTENSION));
+                return (!in_array($type, $params[0]));
+            }
             return true;
         }
     }
+    return true;
 }, $this->_('is of one of the forbidden file types: %s'));
 
 /**
  * Check if entered string is a valid time
  */
-$this->Validator::addRule('time', function ($value) {
+V::addRule('time', function ($field, $value) {
     if ($value) {
         $time_regex = '#^([01]?[0-9]|2[0-3]):[0-5][0-9](:[0-5][0-9])?$#'; //regex for 24-hour format
         return (preg_match($time_regex, $value));
