b6cee8f2020cc8a60a03b67ab5b58d3656164945
juergenweb <juergenweb@users.noreply.github.com>
2023-05-22 20:26:37 +0200

    Change max length of password to 128

--- Formelements/Inputelements/Inputfields.php
+++ Formelements/Inputelements/Inputfields.php
@@ -1093,7 +1093,7 @@ abstract class Inputfields extends Element
             }
 
             // Concatenate all look ahead to working regex string and return it
-            return implode('', $regex_parameters) . '.{' . $length . ', 256}$';
+            return implode('', $regex_parameters) . '.{' . $length . ', 128}$';
         }
         return null;
     }
@@ -1516,5 +1516,6 @@ abstract class Inputfields extends Element
         $this->removeAttribute('min');
         $this->removeAttribute('max');
     }
-    
+
+
 }
