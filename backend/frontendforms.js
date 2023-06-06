5d576e33b85eec13773c5d42fdbfedb1b41b6399
juergenweb <juergenweb@users.noreply.github.com>
2023-06-06 13:35:04 +0200

    Add function to open fieldset on click of a link

--- backend/frontendforms.js
+++ backend/frontendforms.js
@@ -7,7 +7,32 @@ File name: frontendforms.js
 Created: 17.07.2022 
 */
 
+/*jslint browser:true */
+
+function openFieldset(event){
+
+    const fieldset_spam = document.getElementById("spam");
+    const fieldset_attempts = document.getElementById("failed-attempts");
+
+    if(fieldset_attempts){
+        if(fieldset_attempts.classList.contains("InputfieldStateCollapsed")){
+            // open the fieldset by removing InputfieldStateCollapsed class attribute
+            fieldset_spam.classList.remove("InputfieldStateCollapsed");
+            fieldset_attempts.classList.remove("InputfieldStateCollapsed");
+        }
+    }
+}
 
 
+window.onload = function () {
 
+    const loggin_enable_link = document.getElementById("enable-logging");
 
+    if(loggin_enable_link){
+
+        // add event listener
+        loggin_enable_link.addEventListener("click", openFieldset);
+
+    }
+
+};
