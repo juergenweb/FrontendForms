0f2353804ba51a7f15ba145a7cfc1ca78494a24d
juergenweb <juergenweb@users.noreply.github.com>
2023-10-22 19:30:04 +0200

    Fixing bug, that toggle checkbox is always visible

--- Formelements/Inputelements/Inputs/InputPassword.php
+++ Formelements/Inputelements/Inputs/InputPassword.php
@@ -1,148 +1,158 @@
 <?php
-declare(strict_types=1);
+    declare(strict_types=1);
 
-namespace FrontendForms;
+    namespace FrontendForms;
 
-/*
- * Class for creating an input password element
- *
- * Created by Jürgen K.
- * https://github.com/juergenweb
- * File name: InputPassword.php
- * Created: 03.07.2022
- */
-
-use Exception;
-use ProcessWire\WireException;
-use ProcessWire\WirePermissionException;
-
-class InputPassword extends InputText
-{
-    protected $passwordField; // the password field object
-    protected $passwordModule; // the password fieldtype module
-    protected string $passwordFieldName = 'pass';
-    protected bool $showPasswordRequirements = false;
-    protected int|string $minLength = '6'; // the min length of the password as set inside the input field configuration
-    protected InputCheckbox $showPasswordToggle; // The password toggle checkbox object
-
-    /**
-     * @param string $id
-     * @throws WireException
-     * @throws WirePermissionException
-     * @throws Exception
+    /*
+     * Class for creating an input password element
+     *
+     * Created by Jürgen K.
+     * https://github.com/juergenweb
+     * File name: InputPassword.php
+     * Created: 03.07.2022
      */
-    public function __construct(string $id)
-    {
-        parent::__construct($id);
-        $this->setAttribute('type', 'password');
-        $this->passwordField = $this->wire('fields')->get($this->passwordFieldName);
-        $this->passwordModule = $this->wire('modules')->get('InputfieldPassword');
-        $this->minLength = $this->getMinLength();
-        // add meetsPasswordConditions Validator by default
-        $this->setRule('meetsPasswordConditions');
 
-    }
+    use Exception;
+    use ProcessWire\WireException;
+    use ProcessWire\WirePermissionException;
 
-    /**
-     * Get the minimum password length from the password field configuration in the backend
-     * @return string
-     * @throws WireException
-     * @throws WirePermissionException
-     */
-    protected function getMinLength(): string
+    class InputPassword extends InputText
     {
-        // get the default min length value as set in the input field if present
-        if ($this->passwordField->minlength) {
-            $length = $this->passwordField->minlength;
-        } else {
-            // get the default value from the module
-            $length = $this->passwordModule->minlength;
+        protected $passwordField; // the password field object
+        protected $passwordModule; // the password fieldtype module
+        protected string $passwordFieldName = 'pass';
+        protected bool $showPasswordRequirements = false;
+        protected int|string $minLength = '6'; // the min length of the password as set inside the input field configuration
+        protected InputCheckbox $createPasswordToggle; // The password toggle checkbox object
+        protected bool $showPasswordToggle = true; // show or hide the password toggle checkbox
+
+        /**
+         * @param string $id
+         * @throws WireException
+         * @throws WirePermissionException
+         * @throws Exception
+         */
+        public function __construct(string $id)
+        {
+            parent::__construct($id);
+            $this->setAttribute('type', 'password');
+            $this->passwordField = $this->wire('fields')->get($this->passwordFieldName);
+            $this->passwordModule = $this->wire('modules')->get('InputfieldPassword');
+            $this->minLength = $this->getMinLength();
+            // add meetsPasswordConditions Validator by default
+            $this->setRule('meetsPasswordConditions');
         }
-        return (string)$length;
-    }
 
-    /**
-     * Show the password requirements at the password field
-     * @return void
-     */
-    public function showPasswordRequirements(): void
-    {
-        $this->showPasswordRequirements = true;
-    }
-
-    /**
-     * Add a password toggle checkbox to the input element
-     * @return InputCheckbox
-     */
-    public function showPasswordToggle(): InputCheckbox
-    {
-        $toggle = new InputCheckbox('pwtoggle');
-        $toggle->setLabel($this->_('Show password'))->setAttribute('class', 'pwtoggleLabel');
-        $toggle->setAttribute('class', 'pwtoggle');
-        $toggle->useInputWrapper(false);
-        $toggle->useFieldWrapper(false);
-        $toggle->removeAttribute('id');
-        $this->showPasswordToggle = $toggle;
-        return $toggle;
-    }
-
-    /**
-     * Render the password input element
-     * @return string
-     * @throws WireException
-     * @throws WirePermissionException
-     */
-    public function ___renderInputPassword(): string
-    {
-        if ($this->showPasswordRequirements) {
-            if ($this->getDescription()) {
-                $this->setDescription($this->renderPasswordRequirements() . '<br>' . $this->getDescription()->getText());
+        /**
+         * Get the minimum password length from the password field configuration in the backend
+         * @return string
+         */
+        protected function getMinLength(): string
+        {
+            // get the default min length value as set in the input field if present
+            if ($this->passwordField->minlength) {
+                $length = $this->passwordField->minlength;
             } else {
-                $this->setDescription($this->renderPasswordRequirements());
+                // get the default value from the module
+                $length = $this->passwordModule->minlength;
+            }
+            return (string)$length;
+        }
+
+        /**
+         * Show the password requirements at the password field
+         * @return \FrontendForms\InputPassword
+         */
+        public function showPasswordRequirements(): self
+        {
+            $this->showPasswordRequirements = true;
+            return $this;
+        }
+
+        /**
+         * Show (default) or hide the toggle checkbox for the password field
+         * @param bool $show
+         * @return $this
+         */
+        public function showPasswordToggle(bool $show = true): self
+        {
+            $this->showPasswordToggle = $show;
+            return $this;
+        }
+
+        /**
+         * Create a password toggle checkbox to the input element
+         * @return InputCheckbox
+         */
+        protected function createPasswordToggle(): InputCheckbox
+        {
+            $toggle = new InputCheckbox('pwtoggle');
+            $toggle->setLabel($this->_('Show password'))->setAttribute('class', 'pwtoggleLabel');
+            $toggle->setAttribute('class', 'pwtoggle');
+            $toggle->useInputWrapper(false);
+            $toggle->useFieldWrapper(false);
+            $toggle->removeAttribute('id');
+            $this->createPasswordToggle = $toggle;
+            return $toggle;
+        }
+
+        /**
+         * Render the password input element
+         * @return string
+         * @throws WireException
+         * @throws WirePermissionException
+         */
+        public function ___renderInputPassword(): string
+        {
+            if ($this->showPasswordRequirements) {
+                if ($this->getDescription()) {
+                    $this->setDescription($this->renderPasswordRequirements() . '<br>' . $this->getDescription()->getText());
+                } else {
+                    $this->setDescription($this->renderPasswordRequirements());
+                }
+            }
+            if ($this->showPasswordToggle) {
+                $this->append($this->createPasswordToggle()->___render());
+            }
+            return parent::___renderInputText();
+        }
+
+        /**
+         * Render a text which informs about the requirements of a password as set in the backend
+         * @return string|null
+         * @throws WireException
+         * @throws WirePermissionException
+         */
+        public function renderPasswordRequirements(): ?string
+        {
+            if ($this->getPasswordConditions()) {
+                return sprintf($this->_('The password must be at least %s characters and must contain characters of the following categories: %s.'),
+                    (string)$this->minlength, $this->getPasswordConditions());
+            } else {
+                if ($this->minlength) {
+                    return sprintf($this->_('The password must be at least %s characters.'), (string)$this->minlength);
+                }
+                return null;
             }
         }
 
-        $this->append($this->showPasswordToggle()->___render());
-
-        return parent::___renderInputText();
-    }
-
-    /**
-     * Render a text which informs about the requirements of a password as set in the backend
-     * @return string|null
-     * @throws WireException
-     * @throws WirePermissionException
-     */
-    public function renderPasswordRequirements(): ?string
-    {
-        if ($this->getPasswordConditions()) {
-            return sprintf($this->_('The password must be at least %s characters and must contain characters of the following categories: %s.'),
-                (string)$this->minlength, $this->getPasswordConditions());
-        } else {
-            if ($this->minlength) {
-                return sprintf($this->_('The password must be at least %s characters.'), (string)$this->minlength);
+        /**
+         * Show a hint for requirements for the password as set in the backend
+         * @return string|null - returns a comma-separated list of all conditions or null if no conditions were set
+         * @throws WireException
+         * @throws WirePermissionException
+         */
+        protected function getPasswordConditions(): ?string
+        {
+            $passwordModule = $this->wire('modules')->get('InputfieldPassword');
+            $requirements = array_unique(array_merge($passwordModule->requirements, (array)$this->passwordField->requirements));
+            if (in_array('none', $requirements)) {
+                return null;
             }
-            return null;
+            $conditions = [];
+            foreach ($requirements as $name) {
+                $conditions[] = $passwordModule->requirementsLabels[$name];
+            }
+            return implode(', ', $conditions);
         }
     }
-
-    /**
-     * Show a hint for requirements for the password as set in the backend
-     * @return string|null - returns a comma separated list of all conditions or null if no conditions were set
-     * @throws WireException
-     * @throws WirePermissionException
-     */
-    protected function getPasswordConditions(): ?string
-    {
-        $passwordModule = $this->wire('modules')->get('InputfieldPassword');
-        $requirements = array_unique(array_merge($passwordModule->requirements, (array)$this->passwordField->requirements));
-        if (in_array('none', $requirements)) {
-            return null;
-        }
-        $conditions = [];
-        foreach ($requirements as $name) {
-            $conditions[] = $passwordModule->requirementsLabels[$name];
-        }
-        return implode(', ', $conditions);
-    }
-}
