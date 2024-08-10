749c087c87dbdf9dbfc91ef2010ad7edc1a4644e
juergenweb <juergenweb@users.noreply.github.com>
2024-08-10 16:04:59 +0200

    Geo-location data added

--- detail-view.php
+++ detail-view.php
@@ -1,26 +1,30 @@
 <?php
-declare(strict_types=1);
+    declare(strict_types=1);
 
 // set user language
 
-/*
- * Template for detail view which will be displayed inside the modal panel
- *
- * Created by Jürgen K.
- * https://github.com/juergenweb 
- * File name: details.php
- * Created: 18.07.2022 
- */
+    /*
+     * Template for detail view which will be displayed inside the modal panel
+     *
+     * Created by Jürgen K.
+     * https://github.com/juergenweb
+     * File name: details.php
+     * Created: 18.07.2022
+     */
 
-$out = '<!DOCTYPE html>
+    $out = '<!DOCTYPE html>
 <html>
 <head>';
-$css = $this->wire('config')->urls->modules . 'Markup/MarkupAdminDataTable/MarkupAdminDataTable.css';
-$out .= '<link rel="stylesheet" href="' . $css . '">';
-$js = $this->wire('config')->urls->modules . 'Markup/MarkupAdminDataTable/MarkupAdminDataTable.js';
-$out .= '<script type="text/javascript" src="' . $js . '"></script>';
+    $css = $this->wire('config')->urls->modules . 'Markup/MarkupAdminDataTable/MarkupAdminDataTable.css';
+    $out .= '<link rel="stylesheet" href="' . $css . '">';
+    $js = $this->wire('config')->urls->modules . 'Markup/MarkupAdminDataTable/MarkupAdminDataTable.js';
+    $out .= '<script type="text/javascript" src="' . $js . '"></script>';
 
-$out .= '<style>
+    $out .= '<style>
+        body#detail-view{
+            padding:10px;
+        }
+        
         #linkwrapper {
             text-align: center;
             padding: 15px;
@@ -50,13 +54,46 @@ $out .= '<style>
             padding-bottom: 12px;
             text-align: left;
         }
+        
+        dl {
+    border: 3px double #ccc;
+    padding: 0.5em;
+  }
+  dt {
+    float: left;
+    clear: left;
+    width: 100px;
+    text-align: right;
+    font-weight: bold;
+    color: green;
+  }
+  dt::after {
+    content: ":";
+  }
+  dd {
+    margin: 0 0 0 110px;
+    padding: 0 0 0.5em 0;
+  }
     </style>
 
 </head>
-<body>';
-$url = strtok($_SERVER["REQUEST_URI"], '?');
-$ip = basename($url);
-$out .= $this->executeWhoisLookUp($ip);
-$out .= $this->executeViewDetailsTable($ip);
-$out .= '</body>
+<body id="detail-view">';
+    $url = strtok($_SERVER["REQUEST_URI"], '?');
+    $ip = basename($url);
+    $ipdata = \ProcessWire\FrontendForms::getIPData($ip);
+    $out .= '<h1>'.sprintf($this->_('More information about %s'), $ip).'</h1>';
+    if ($ipdata->status == 'success') {
+        $out .= '<h2>'.$this->_('Geo location').'</h2>';
+        $out .= '<dl id="detail-view-geo">';
+        $out .= '<dt>' . $this->_('Country') . '</dt><dd>' . $ipdata->country . '</dd>';
+        $out .= '<dt>' . $this->_('Region') . '</dt><dd>' . $ipdata->regionName . '</dd>';
+        $out .= '<dt>' . $this->_('City') . '</dt><dd>' . $ipdata->city . '</dd>';
+        $out .= '</dl>';
+    } else {
+        $this->_('We are sorry, but ip-api.com does not have any information about this IP address.');
+    }
+
+    $out .= $this->executeWhoisLookUp($ip);
+    $out .= $this->executeViewDetailsTable($ip);
+    $out .= '</body>
 </html>';
