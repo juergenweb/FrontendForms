1535dfda2e19793a9ece27aa693323f86a23e3d3
juergenweb <juergenweb@users.noreply.github.com>
2025-01-03 19:09:29 +0100

    Update Privacy.php to support external URL

--- Formelements/Inputelements/Inputs/defaults/Privacy.php
+++ Formelements/Inputelements/Inputs/defaults/Privacy.php
@@ -1,76 +1,89 @@
 <?php
-declare(strict_types=1);
+    declare(strict_types=1);
 
-namespace FrontendForms;
+    namespace FrontendForms;
 
-/*
- * Class with pre-defined values for creating an "Accept our data privacy" input field
- *
- * Created by Jürgen K.
- * https://github.com/juergenweb
- * File name: Privacy.php
- * Created: 03.07.2022
- */
-
-use Exception;
-use ProcessWire\WireException;
-use ProcessWire\WirePermissionException;
-
-class Privacy extends InputCheckbox
-{
-
-    protected Link $privacyLink;
-
-    /**
-     * @param string $id
-     * @throws WireException
-     * @throws WirePermissionException
-     * @throws Exception
+    /*
+     * Class with pre-defined values for creating an "Accept our data privacy" input field
+     *
+     * Created by Jürgen K.
+     * https://github.com/juergenweb
+     * File name: Privacy.php
+     * Created: 03.07.2022
      */
-    public function __construct(string $id)
-    {
-        parent::__construct($id);
-        $this->setLabel($this->_('I accept the privacy policy'));
-        $this->setRule('required')->setCustomMessage($this->_('You have to accept our privacy policy'));
 
-        // create the link to the privacy page if set
-        if($this->frontendforms['input_privacy']){
-            $privacyPage = $this->wire('pages')->get($this->frontendforms['input_privacy']); // grab the privacy page
+    use Exception;
+    use ProcessWire\WireException;
+    use ProcessWire\WirePermissionException;
+
+    class Privacy extends InputCheckbox
+    {
+
+        protected Link $privacyLink;
+
+        /**
+         * @param string $id
+         * @throws WireException
+         * @throws WirePermissionException
+         * @throws Exception
+         */
+        public function __construct(string $id)
+        {
+            parent::__construct($id);
+            $this->setLabel($this->_('I accept the privacy policy'));
+            $this->setRule('required')->setCustomMessage($this->_('You have to accept our privacy policy'));
+
+            if (!array_key_exists('input_privacypageselect', $this->frontendforms))
+                $this->frontendforms['input_privacypageselect'] = 'int';
+
+            // create the link to the privacy page if set
             $this->privacyLink = new Link();
-            $this->privacyLink->setPageLink($privacyPage);
             $this->privacyLink->setLinkText($this->_('Privacy Policy'));
             $this->privacyLink->setAttribute('title', $this->_('To the Privacy Policy page'));
-            $this->setLabel($this->getLabel()->getText() . ' (' . $this->privacyLink->___render() . ')');
-       }
+            $linkExists = false;
 
-   }
+            if ($this->frontendforms['input_privacypageselect'] === 'int' && $this->frontendforms['input_privacy']) {
+                $linkExists = true;
+                $privacyPage = $this->wire('pages')->get($this->frontendforms['input_privacy']); // grab the privacy page
+                $this->privacyLink->setPageLink($privacyPage);
+            }
 
-   /**
-    * Method to set the url where you can find the privacy policy
-    * @param string|null $privacyUrl
-    * @return Link
-    */
-    public function setPrivacyUrl(?string $privacyUrl = null): Link
-    {
-        return $this->privacyLink->setUrl($privacyUrl);
+            if ($this->frontendforms['input_privacypageselect'] === 'ext' && $this->frontendforms['input_privacypageurl']) {
+                $linkExists = true;
+                $this->privacyLink->setUrl($this->frontendforms['input_privacypageurl']);
+            }
+
+            if ($linkExists) {
+                $this->setLabel($this->getLabel()->getText() . ' (' . $this->privacyLink->___render() . ')');
+            }
+        }
+
+        /**
+         * Method to set the url where you can find the privacy policy
+         * @param string|null $privacyUrl
+         * @return Link
+         */
+        public function setPrivacyUrl(?string $privacyUrl = null): Link
+        {
+            return $this->privacyLink->setUrl($privacyUrl);
+        }
+
+        /**
+         * Get the url of the page where you can find the privacy policy
+         * @return string|null
+         */
+        public function getPrivacyUrl(): ?string
+        {
+            return $this->privacyLink->getUrl();
+        }
+
+        /**
+         * Render the privacy checkbox
+         * @return string
+         */
+        public function ___renderPrivacy(): string
+        {
+            return parent::___renderInputCheckbox();
+        }
+
     }
-
-    /**
-     * Get the url of the page where you can find the privacy policy
-     * @return string|null
-     */
-    public function getPrivacyUrl(): ?string
-    {
-        return $this->privacyLink->getUrl();
-    }
-
-    /**
-     * Render the privacy checkbox
-     * @return string
-     */
-    public function ___renderPrivacy(): string
-    {
-        return parent::___renderInputCheckbox();
-    }
-
-}
