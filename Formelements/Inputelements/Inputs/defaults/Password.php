a1b4b1b1976fcffa06017aebb647aa6e16d7369e
juergenweb <juergenweb@users.noreply.github.com>
2025-01-31 14:23:07 +0100

    Removed 3 underscores from hookable function calls to re-enable hooking.

--- Formelements/Inputelements/Inputs/defaults/Password.php
+++ Formelements/Inputelements/Inputs/defaults/Password.php
@@ -1,47 +1,47 @@
 <?php
-declare(strict_types=1);
+    declare(strict_types=1);
 
-namespace FrontendForms;
+    namespace FrontendForms;
 
-/*
- * Class with pre-defined values for creating a password input field
- *
- * Created by Jürgen K.
- * https://github.com/juergenweb
- * File name: Password.php
- * Created: 03.07.2022
- */
-
-use Exception;
-use ProcessWire\WireException;
-use ProcessWire\WirePermissionException;
-
-class Password extends InputPassword
-{
-
-    /**
-     * @param string $id
-     * @throws Exception
+    /*
+     * Class with pre-defined values for creating a password input field
+     *
+     * Created by Jürgen K.
+     * https://github.com/juergenweb
+     * File name: Password.php
+     * Created: 03.07.2022
      */
-    public function __construct(string $id)
-    {
-        parent::__construct($id);
-        $this->setLabel($this->_('Password')); // set default label
-        $this->setRule('required');
-        $this->setRule('safePassword');
-        $this->setRule('lengthMin', $this->minLength);
-        $this->setRule('lengthMax', '128');
-    }
 
-    /**
-     * @return string
-     * @throws WireException
-     * @throws WirePermissionException
-     */
-    public function ___renderPassword(): string
+    use Exception;
+    use ProcessWire\WireException;
+    use ProcessWire\WirePermissionException;
+
+    class Password extends InputPassword
     {
 
-        return parent::___renderInputPassword();
-    }
+        /**
+         * @param string $id
+         * @throws Exception
+         */
+        public function __construct(string $id)
+        {
+            parent::__construct($id);
+            $this->setLabel($this->_('Password')); // set default label
+            $this->setRule('required');
+            $this->setRule('safePassword');
+            $this->setRule('lengthMin', $this->minLength);
+            $this->setRule('lengthMax', '128');
+        }
 
-}
+        /**
+         * @return string
+         * @throws WireException
+         * @throws WirePermissionException
+         */
+        public function ___renderPassword(): string
+        {
+
+            return parent::renderInputPassword();
+        }
+
+    }
