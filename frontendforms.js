da303c44e53ceadd9b6a3ea4f71c41b7774211ca
juergenweb <juergenweb@users.noreply.github.com>
2025-09-22 17:19:14 +0200

    Update frontendforms.js

--- frontendforms.js
+++ frontendforms.js
@@ -14,10 +14,8 @@ window.addEventListener("DOMContentLoaded", function () {
         const k = 1024;
         const dm = decimals < 0 ? 0 : decimals;
         const sizes = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
-
         const i = Math.floor(Math.log(bytes) / Math.log(k));
-
-        return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`;
+        return parseFloat((bytes / Math.pow(k, i)).toFixed(dm))+ ' ' + sizes[i];
     }
 
     /*
