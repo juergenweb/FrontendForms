b7e9209be77e1915d82b04dac5f10e3091b8e849
juergenweb <juergenweb@users.noreply.github.com>
2026-05-05 22:26:40 +0200

    Optimized via Claude AI

--- Formelements/Inputelements/Select/Select.php
+++ Formelements/Inputelements/Select/Select.php
@@ -1,15 +1,17 @@
 <?php
+
 declare(strict_types=1);
 
 namespace FrontendForms;
 
 /*
- * Class for creating a select element
+ * Class for creating a select element.
  *
  * Created by Jürgen K.
  * https://github.com/juergenweb
  * File name: Select.php
  * Created: 03.07.2022
+ * Optimized via Claude AI 05.05.26
  */
 
 use ProcessWire\WireException;
@@ -17,13 +19,11 @@ use ProcessWire\WirePermissionException;
 
 class Select extends Inputfields
 {
-
     use TraitOption, TraitPWOptions, TraitOptionElements, TraitInputfields;
 
-    protected ?Wrapper $selectWrapper = null; // special wrapper for Bulma framework
+    protected ?Wrapper $selectWrapper = null;
 
     /**
-     * @param string $id
      * @throws WireException
      * @throws WirePermissionException
      */
@@ -33,13 +33,12 @@ class Select extends Inputfields
         $this->setTag('select');
         $this->setCSSClass('selectClass');
 
-        if($this->frontendforms['input_framework'] === 'bulma1.json') {
+        if ($this->frontendforms['input_framework'] === 'bulma1.json') {
             $this->selectWrapper = new Wrapper();
             $this->selectWrapper->setTag('div');
             $this->selectWrapper->setAttribute('class', 'select');
 
-            // get class
-            if(get_class($this) == 'FrontendForms\SelectMultiple') {
+            if (is_a($this, SelectMultiple::class)) {
                 $this->selectWrapper->setAttribute('class', 'is-multiple');
             }
         }
@@ -81,39 +80,42 @@ class Select extends Inputfields
      */
     public function ___renderSelect(): string
     {
-        $out = '';
-        if ($this->options) {
-            $options = '';
-            foreach ($this->options as $option) {
-                // check if option element is hr element
-                if($option instanceof TextElements) {
-                    // add hr tag only to Select, but not to SelectMultiple
-                    if($option->getTag() === 'hr'){
-                        $options .= $option->renderSelfclosingTag($option->getTag());
-                    }
-                } else {
-                    if (in_array($option->getAttribute('value'), $this->getDefaultValue())) {
-                        $option->setAttribute('selected');
-                        if($this->useAriaAttr) $option->setAttribute('aria-selected', 'true');
-                    }
-                    if (in_array($option->getAttribute('value'), (array)$this->getPostValue())) {
-                        $option->setAttribute('selected');
-                        if($this->useAriaAttr) $option->setAttribute('aria-selected', 'true');
-                    }
-                    $options .= $option->render();
+        if (empty($this->options)) {
+            return '';
+        }
+
+        $selectedValues = array_merge(
+            (array) $this->getDefaultValue(),
+            (array) $this->getPostValue()
+        );
+
+        $options = '';
+        foreach ($this->options as $option) {
+            if ($option instanceof TextElements) {
+                if ($option->getTag() === 'hr') {
+                    $options .= $option->renderSelfclosingTag($option->getTag());
+                }
+                continue;
+            }
+
+            if (in_array($option->getAttribute('value'), $selectedValues, strict: true)) {
+                $option->setAttribute('selected');
+                if ($this->useAriaAttr) {
+                    $option->setAttribute('aria-selected', 'true');
                 }
             }
-            $this->setContent($options);
 
-            $out = $this->renderNonSelfclosingTag($this->getTag());
-
-            // special treatment for Bulma framework
-            if($this->frontendforms['input_framework'] === 'bulma1.json') {
-                $this->selectWrapper->setContent($out);
-                $out = $this->selectWrapper->renderNonSelfclosingTag($this->selectWrapper->getTag());
-            }
+            $options .= $option->render();
         }
+
+        $this->setContent($options);
+        $out = $this->renderNonSelfclosingTag($this->getTag());
+
+        if ($this->frontendforms['input_framework'] === 'bulma1.json') {
+            $this->selectWrapper->setContent($out);
+            $out = $this->selectWrapper->renderNonSelfclosingTag($this->selectWrapper->getTag());
+        }
+
         return $out;
     }
-
 }
